67a4d0450fb35e2492ad5784b60d414cbdbcb03a
Lindsay Boyden <lindsayboyden@yahoo.com>
2020-05-22 14:30:04 -0600

    forced login startup, and working logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
   def index
+    @users = current_user.users
   end
 
   def show
   end
+
+  def new
+    # render partial: 'form'
+  end
+
+  def create
+  end
+
+  def edit
+    # render partial: 'form'
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private 
+
+
 end
